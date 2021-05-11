70a412d0a6cdef7df052d79930027cb73e8eec4a
Lori <erica.iida14@gmail.com>
2021-05-11 14:00:56 -0400

    made Board and Piece class

---
+++ Chess/Board.rb
@@ -0,0 +1,8 @@
+class Board
+
+    def initialize
+        @grid = Array.new(8) { Array.new(8) }
+
+    end 
+
+end 
---
+++ Chess/Piece.rb
@@ -0,0 +1,11 @@
+class Piece
+    def initialize(color, board, pos = [])
+        @color = color
+        @board = board
+        @pos = pos
+    end 
+
+    #go back later
+    def empty?
+    end
+end 
